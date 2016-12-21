7d6e969753e3ebc517421966a9cc6b466ca464fe
Patrick Cullen <patrick.cullen@washpost.com>
2016-12-21 12:16:13 -0500

    added location conf parsing

--- src/ngx_http_lambda_module.c
+++ src/ngx_http_lambda_module.c
@@ -4,22 +4,21 @@
 
 static char *ngx_http_lambda(ngx_conf_t *cf, ngx_command_t *cmd, void *conf);
 static ngx_int_t ngx_http_lambda_handler(ngx_http_request_t *req);
+static void * ngx_http_lambda_create_loc_conf(ngx_conf_t *cf);
 
 /**
  * This module provided directive: lambda.
  *
  */
 static ngx_command_t ngx_http_lambda_commands[] = {
-
     { 
-      ngx_string("lambda"), /* directive */
-      NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1, /* location context and takes 1 argument the lambda function ARN */
-      ngx_http_lambda, /* configuration setup function */
-      0, /* No offset. Only one context is supported. */
-      0, /* No offset when storing the module configuration on struct. */
-      NULL
+        ngx_string("lambda"), /* directive */
+        NGX_HTTP_LOC_CONF|NGX_CONF_TAKE1, /* location context and takes 1 argument the lambda function ARN */
+        ngx_http_lambda, /* configuration setup function */
+        0, /* No offset. Only one context is supported. */
+        0, /* No offset when storing the module configuration on struct. */
+        NULL
     },
-
     ngx_null_command /* command termination */
 };
 
@@ -34,7 +33,7 @@ static ngx_http_module_t ngx_http_lambda_module_ctx = {
     NULL, /* create server configuration */
     NULL, /* merge server configuration */
 
-    NULL, /* create location configuration */
+    ngx_http_lambda_create_loc_conf, /* create location configuration */
     NULL /* merge location configuration */
 };
 
@@ -54,6 +53,21 @@ ngx_module_t ngx_http_lambda_module = {
     NGX_MODULE_V1_PADDING
 };
 
+typedef struct {
+    ngx_str_t arn;
+} ngx_http_lambda_loc_conf_t;
+
+static void * ngx_http_lambda_create_loc_conf(ngx_conf_t *cf)
+{
+    ngx_http_core_loc_conf_t * conf;
+    conf = ngx_pcalloc(cf->pool, sizeof(ngx_http_lambda_loc_conf_t));
+    if (conf == NULL) {
+        return NULL;
+    }
+
+    return conf;
+}
+
 /**
  * Configuration setup function that installs the content handler.
  *
@@ -66,7 +80,7 @@ ngx_module_t ngx_http_lambda_module = {
  * @return string
  *   Status of the configuration setup.
  */
-static char *ngx_http_lambda(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
+static char * ngx_http_lambda(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
 {
     ngx_http_core_loc_conf_t *clcf; /* pointer to core location configuration */
 
@@ -77,7 +91,7 @@ static char *ngx_http_lambda(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     return NGX_CONF_OK;
 } 
 
-static u_char ngx_lambda[] = "hello world";
+static u_char ngx_lambda_stub[] = "{\"statusCode\": 200, \"headers\": { \"Host\": \"localhost\" }, \"body\": \"hello world\" }";
 
 /**
  * Content handler.
@@ -92,9 +106,14 @@ static ngx_int_t ngx_http_lambda_handler(ngx_http_request_t *req)
     ngx_buf_t *buf;
     ngx_chain_t out;
 
+    /* we response to 'GET' requests only, for now */
+    if (!(req->method & NGX_HTTP_GET)) {
+        return NGX_HTTP_NOT_ALLOWED;
+    }
+
     /* Set the Content-Type header. */
-    req->headers_out.content_type.len = sizeof("text/html") - 1;
-    req->headers_out.content_type.data = (u_char *) "text/html";
+    req->headers_out.content_type.len = sizeof("application/json") - 1;
+    req->headers_out.content_type.data = (u_char *) "application/json";
 
     /* Allocate a new buffer for sending out the reply. */
     buf = ngx_pcalloc(req->pool, sizeof(ngx_buf_t));
@@ -103,15 +122,15 @@ static ngx_int_t ngx_http_lambda_handler(ngx_http_request_t *req)
     out.buf = buf;
     out.next = NULL; /* just one buffer */
 
-    buf->pos = ngx_lambda; /* first position in memory of the data */
-    buf->last = ngx_lambda + sizeof(ngx_lambda); /* last position in memory of the data */
+    buf->pos = ngx_lambda_stub; /* first position in memory of the data */
+    buf->last = ngx_lambda_stub + sizeof(ngx_lambda_stub); /* last position in memory of the data */
     buf->memory = 1; /* content is in read-only memory */
     buf->last_buf = 1; /* there will be no more buffers in the request */
 
     /* Sending the headers for the reply. */
     req->headers_out.status = NGX_HTTP_OK; /* 200 status code */
     /* Get the content length of the body. */
-    req->headers_out.content_length_n = sizeof(ngx_lambda);
+    req->headers_out.content_length_n = sizeof(ngx_lambda_stub);
     ngx_http_send_header(req); /* Send the headers */
 
     /* Send the body, and return the status code of the output filter chain. */
